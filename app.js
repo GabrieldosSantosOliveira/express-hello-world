b34b0b54e2308c7764ef2df897aa264fb86d12cd
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-20 13:07:55 -0400

    style: Formatted with prettier

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => res.type("html").send(html));
 
-app.get('/req', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+app.get("/req", (req, res) => {
+  console.log("Just got a request!");
+  res.send("Yo!");
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
 
 const html = `
 <!DOCTYPE html>
@@ -61,4 +60,4 @@ const html = `
     </section>
   </body>
 </html>
-`
+`;
